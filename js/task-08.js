0421a19077c52d85b54364255b2b84f8704f58f9
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-09-14 18:41:54 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    
+    const formEmail = loginForm.elements.email;
+    const formPassword = loginForm.elements.password;
+    let formData;
+    
+    if(formEmail.value === '' && formPassword.value === ''){
+    alert('Будь ласка, заповніть всі поля');
+} else{
+    formData ={
+        email: formEmail.value,
+        password: formPassword.value
+    }
+}
+
+console.log(formData);
+loginForm.reset()
+})
